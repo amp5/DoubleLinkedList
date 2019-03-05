fd88dae1fd506f058c5b99f6a7b03fc6b11662ea
Alexandra Plassaras <v-alplas@microsoft.com>
2019-03-05 15:28:39 -0800

    Add project files.

---
+++ DoubleLinkedList/doublelinkedlist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoubleLinkedList
+{
+    class DoubleLinkedList
+    {
+        list doubleLinkedList = List();
+
+        doubleLinkedList.AddtoList(Node());
+    }
+}
---
+++ DoubleLinkedList/node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoubleLinkedList
+{
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+            Next = Previous = null;
+        }
+    }
+}
